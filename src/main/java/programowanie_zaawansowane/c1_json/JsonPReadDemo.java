bb5e23c7e6aa2880dd9d3b5b0ede18cb5cfdf921
cs <siwon.cezary@gmail.com>
2019-01-29 22:54:23 +0100

    json

--- src/main/java/programowanie_zaawansowane/c1_json/JsonPReadDemo.java
+++ src/main/java/programowanie_zaawansowane/c1_json/JsonPReadDemo.java
@@ -14,7 +14,7 @@ public class JsonPReadDemo {
       URL url = new URL("https://api-v3.mojepanstwo.pl/dane/zamowienia_publiczne.json");
       //tworzymy fabrykę readera
       JsonReaderFactory readerFactory = Json.createReaderFactory(Collections.emptyMap());
-      //uzyskujemy readera odczytującego ze strumienia url-a
+      //uzyskujemy readera odczytującego dane ze strumienia url-a
       JsonReader jsonReader = readerFactory.createReader(url.openStream());
       //odczytanie danych w json do obiektu klasy JsonObject
       JsonObject jsonObj = jsonReader.readObject();
@@ -24,7 +24,7 @@ public class JsonPReadDemo {
           "Dataobject":                                               nazwa obiektu
           [                                                           tablica
             {                                                         obiekt bez nazwy, numer 0
-              "id": "1767324",                                        klucz: wartość typu String
+              "id": "1767324",                                        klucz: wartość
               "dataset": "zamowienie_publiczne",                      klucz: wartość
               ..
               "data":                                                 klucz: wartość typu obiekt
@@ -39,18 +39,18 @@ public class JsonPReadDemo {
        */
       //Odczyt pierwszego obiektu, którego pole o kluczu "data" jest obiektem
       jsonObj
-        .getJsonArray("Dataobject")                 //pobranie tabicy Dataobject
-        .get(0).asJsonObject()                          //pobranie elementu o indeksie 0 z tablicy i konwersja do obiektu
-        .forEach((key, val)-> {                         //dla każdego klucza key o wartości val
-        if (key.equals("data")) {                       //jeśli to pole o kluczu "data"
-            val.asJsonObject()                          //konwersja wartości pola na obiekt
-            .forEach((k, v) -> {                        //dla każdego klucza k i wartości v
-            System.out.println("\t" + k + " :: " + v);  //wyświetlenie pól zagnieżdżonego obiektu
-          });
-        } else {
-          System.out.println(key + " :: " + val);       //wyświetlenie pól obiektu
-        }
-      });
+        .getJsonArray("Dataobject")                   //pobranie tablicy Dataobject
+        .get(0).asJsonObject()                            //pobranie elementu o indeksie 0 i jego konwersja do obiektu
+        .forEach((key, val)-> {                           //dla każdego klucza key o wartości val
+          if (key.equals("data")) {                       //jeśli to pole o kluczu "data" to
+              val.asJsonObject()                              //konwersja wartości pola na obiekt
+              .forEach((k, v) -> {                            //dla każdego klucza k i wartości v
+              System.out.println("\t" + k + " :: " + v);      //wyświetlenie pól zagnieżdżonego obiektu
+            });
+          } else {
+            System.out.println(key + " :: " + val);       //wyświetlenie pól obiektu
+          }
+        });
       } catch (MalformedURLException e) {
         e.printStackTrace();
       } catch (IOException e) {
