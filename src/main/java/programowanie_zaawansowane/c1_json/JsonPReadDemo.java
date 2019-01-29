488249e026e92c429c4ddb85b4b61d1211dcb2d3
cs <siwon.cezary@gmail.com>
2019-01-29 22:47:13 +0100

    json

---
+++ src/main/java/programowanie_zaawansowane/c1_json/JsonPReadDemo.java
@@ -0,0 +1,61 @@
+package programowanie_zaawansowane.c1_json;
+
+import javax.json.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+
+public class JsonPReadDemo {
+  public static void main(String[] args) {
+    try{
+      //Adres url z api generującego dane w json
+      URL url = new URL("https://api-v3.mojepanstwo.pl/dane/zamowienia_publiczne.json");
+      //tworzymy fabrykę readera
+      JsonReaderFactory readerFactory = Json.createReaderFactory(Collections.emptyMap());
+      //uzyskujemy readera odczytującego ze strumienia url-a
+      JsonReader jsonReader = readerFactory.createReader(url.openStream());
+      //odczytanie danych w json do obiektu klasy JsonObject
+      JsonObject jsonObj = jsonReader.readObject();
+      //można teraz odczytać dane z obiektu odwołując się do składowych
+      /*
+        {                                                             obiekt
+          "Dataobject":                                               nazwa obiektu
+          [                                                           tablica
+            {                                                         obiekt bez nazwy, numer 0
+              "id": "1767324",                                        klucz: wartość typu String
+              "dataset": "zamowienie_publiczne",                      klucz: wartość
+              ..
+              "data":                                                 klucz: wartość typu obiekt
+              {                                                       obiekt
+                "zamówienia_publiczne_typy.symbol": "...",            klucz: wartość
+
+              }
+
+            }
+          ]
+        }
+       */
+      //Odczyt pierwszego obiektu, którego pole o kluczu "data" jest obiektem
+      jsonObj
+        .getJsonArray("Dataobject")                 //pobranie tabicy Dataobject
+        .get(0).asJsonObject()                          //pobranie elementu o indeksie 0 z tablicy i konwersja do obiektu
+        .forEach((key, val)-> {                         //dla każdego klucza key o wartości val
+        if (key.equals("data")) {                       //jeśli to pole o kluczu "data"
+            val.asJsonObject()                          //konwersja wartości pola na obiekt
+            .forEach((k, v) -> {                        //dla każdego klucza k i wartości v
+            System.out.println("\t" + k + " :: " + v);  //wyświetlenie pól zagnieżdżonego obiektu
+          });
+        } else {
+          System.out.println(key + " :: " + val);       //wyświetlenie pól obiektu
+        }
+      });
+      } catch (MalformedURLException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+}
---
+++ src/main/java/programowanie_zaawansowane/c1_json/JsonPWriteDemo.java
@@ -0,0 +1,36 @@
+package programowanie_zaawansowane.c1_json;
+
+import javax.json.*;
+import javax.json.stream.JsonGenerator;
+import java.util.Collections;
+import java.util.Map;
+
+public class JsonPWriteDemo {
+  public static void main(String[] args) {
+    JsonBuilderFactory bf = Json.createBuilderFactory(Collections.emptyMap());
+    JsonObject jsonObject =
+      bf.createObjectBuilder()                              //tworzymy obiekt
+      .add("Zajęcia", bf.createArrayBuilder()           //tworzymy pole typu tablica
+          .add(bf.createObjectBuilder()                     //pierwszy obiekt tablicy
+              .add("Tytuł", "Programowanie w Java 1")
+              .add("Data", bf.createObjectBuilder()      //pole Data jest oviektem
+                  .add("rok", 2019)
+                  .add("miesiać", 1)
+                  .add("dzień", 19)
+                  .build())
+              .build())
+          .add(bf.createObjectBuilder()
+              .add("Tytuł", "Programowanie w Java 1")
+              .add("Data", bf.createObjectBuilder()
+                  .add("rok", 2019)
+                  .add("miesiać", 1)
+                  .add("dzień", 19)
+                  .build())
+              .build())
+          .build()
+      ).build();
+    Map<String, ?> config = Collections.singletonMap(JsonGenerator.PRETTY_PRINTING, true);
+    JsonWriterFactory writerFactory = Json.createWriterFactory(config);
+    writerFactory.createWriter(System.out).write(jsonObject);
+  }
+}
