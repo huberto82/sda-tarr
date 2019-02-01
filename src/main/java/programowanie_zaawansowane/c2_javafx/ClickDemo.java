5019811adb1ee221566cad106780da3b1d3d2224
cs <siwon.cezary@gmail.com>
2019-02-01 22:18:25 +0100

    javafx

---
+++ src/main/java/programowanie_zaawansowane/c2_javafx/ClickDemo.java
@@ -0,0 +1,33 @@
+package programowanie_zaawansowane.c2_javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class ClickDemo extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    Pane root = new Pane();
+    Scene scene = new Scene(root, 300,300);
+    root.setOnMouseClicked(event->{
+      Circle circ = new Circle(event.getSceneX(), event.getSceneY(),20);
+      circ.setStroke(Color.SKYBLUE);
+      circ.setFill(Color.GREEN);
+      root.getChildren().add(circ);
+      System.out.println("Click");
+    });
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Click Demo");
+    primaryStage.show();
+  }
+}
